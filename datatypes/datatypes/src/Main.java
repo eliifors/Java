f6f8cd85f422e028c5b7e07283935ef36e026cbd
eliifors <elif.ors@ogr.sakarya.edu.tr>
2026-08-13 18:13:53 +0300

    work

---
+++ datatypes/datatypes/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double sayi = 2.5;
+        sayi = 5;
+
+        char karakter = 'A';
+
+        boolean dogruMu = false;
+    }
+}
