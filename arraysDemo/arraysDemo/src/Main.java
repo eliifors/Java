8cb961869ed1e5023b9480c6736d2e1c0251bd23
eliifors <elif.ors@ogr.sakarya.edu.tr>
2026-08-17 23:18:13 +0300

    arrays

---
+++ arraysDemo/arraysDemo/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String [] ogrenciler = new String[3];
+        ogrenciler[0] = "Elif";
+        ogrenciler[1] = "Kerem";
+        ogrenciler[2] = "Ali";
+
+        for (String ogrenci:ogrenciler){
+            System.out.println(ogrenci);
+        }
+    }
+}
